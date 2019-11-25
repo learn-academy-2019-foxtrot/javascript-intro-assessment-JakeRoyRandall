ed3ec862b06869931f1a75076ca0d57bbd3bc2ab
JakeRoyRandall <57545729+JakeRoyRandall@users.noreply.github.com>
2019-11-24 19:29:58 -0800

    completed assessment

--- js-intro.js
+++ js-intro.js
@@ -2,95 +2,206 @@
 // Coding practical questions
 
 // --------------------------- Consider the following variable:
-
 var mantra = "Be the dev"
 
 // 1a. Write the code that determines if there is a 'B' in mantra.
-
-
+mantra.includes("B")
+//Sanity check that the above line's logic works
+// console.log(mantra.includes("B"));
 
 // 1b. Write the code that determines if there is an 'x' in mantra.
-
-
+mantra.includes("x")
+//Sanity check that the above line's logic works
+// console.log(mantra.includes("x"));
 
 // 1c. Write the code that determines if there is a 'v' in mantra.
-
-
+mantra.includes("v")
+//Sanity check that the above line's logic works
+// console.log(mantra.includes("v"));
 
 // 1d. STRETCH: Write a function that can take two arguments - a string and a letter and returns a sentence telling the user if the letter exists within the string.
-
-
+const ifStringBoolean = (string1, string2) => {
+  if (typeof string1.search(string2) === "number") {
+    return `${string2} exists inside of ${string1}`
+  } else {
+    return `${string2} does not exist inside of ${string1}`
+  }
+}
+//Sanity Checks for ifStringBoolean()
+// console.log(ifStringBoolean(mantra,"B"));
+// console.log(ifStringBoolean(mantra,"x"));
+// console.log(ifStringBoolean(mantra,"v"));
 
 // ------------------------------ Consider the following variables:
-
 var myDog = "Snoopy"
 var myCat = "Garfield"
 
 // 2a. Create a function that returns the longer of the two variables. Expected output: "Garfield"
+const compareStringLength = (string1, string2) => {
+  if (string1.length > string2.length) {
+    return string1
+  } else if (string1.length < string2.length) {
+    return string2
+  } else if (string1 === string2) {
+    return `${string1} and ${string2} are the same length`
+  } else {
+    return "Please enter two valid strings for comparison."
+  }
+}
 
-
-
+// Sanity Checks for compareStringLength()
+// console.log(compareStringLength(myDog, myCat));
+// console.log(compareStringLength(myDog, "short"));
+// console.log(compareStringLength("short", myCat));
+// console.log(compareStringLength(5, myCat));
 
 // 2b. Create a function that takes in two arguments and returns an array containing the two variables. Expected output: ["Snoopy", "Garfield"]
-
-
-
+const toArray = (arg1, arg2) => {
+    return [arg1, arg2]
+}
+//Sanity checks for toArray()
+// console.log(toArray(myDog,myCat));
+// console.log(toArray("items1", [myCat, myDog]));
+// console.log(toArray("jim", 5));
 
 // 2c. STRETCH: Write a function that accepts two variables and returns a string in all lowercase letters. Expected output: "snoopy garfield"
-
-
-
+const twoToLower = (arg1, arg2) => {
+  return `${arg1.toLowerCase()} ${arg2.toLowerCase()}`
+}
+//Sanity checks for twoToLower()
+// console.log(twoToLower(myDog, myCat));
 
 // ------------------------------ Consider the following variable:
-
 var myMessage = "Learning is fun!"
 
 // 3a. Write the code that logs each letter of the message using a for loop.
 //hint: "logs" in this case means "prints to the terminal console."  This is typically done with a console.log(x), where x is the string, function, etc that you wish to see in the terminal.
-
+const letterLog = (string) => {
+  if (typeof string === "string") {
+    for (var i = 0; i < string.length; i++) {
+      console.log(string[i]);
+  }} else {
+    console.log("Please enter a valid string.");
+  }
+  return
+}
+//Sanity checks for letterLog()
+// letterLog("jimbo");
+// letterLog(5);
 
 // 3b. Write the code that logs each letter of the message using map.
-
-
+const letterLogMap = (string) => {
+  if (typeof string === "string") {
+    let newArr = string.split("")
+    return newArr.map((v,i) => {
+    console.log(string[i]);
+  })} else {
+    console.log("Please enter a valid string.");
+  }
+}
+//Sanity checks for letterLog()
+// letterLogMap("jimbo");
+// letterLogMap(5);
 
 // 3c. STRETCH: Write the code that logs each letter of the message using a while loop.
-
-
+// Code commented out for convenience
+  // var i = 0
+  // do {
+  //   console.log(myMessage[i]);
+  //   i++
+  // } while (i < myMessage.length){
+  // }
 
 // 3d. SUPER STRETCH: Write the code that logs each letter of the message using forEach().
-
-
-
+const letterLogEach = (letter) => {
+  console.log(letter);
+}
+// Sanity check for letterLogEach()
+// myMessage.split("").forEach(letterLogEach);
 
 // ---------------------------- Consider the following variable:
-
 var testString = "thisisateststring"
 
 // 4a. Write a function that takes the variable as an argument and returns the string without vowels. Expected output: "thsstststrng"
+const noVowels = (arg1) => {
+  let newArr = arg1.split("")
+  newArr = newArr.filter((v) => {
+    // I couldn't figure out a shorthand using things we learned
+    if (v !== "a" && v!== "e" && v !== "i" && v !== "o" && v !=="u") {
+      return v
+    } else {
+      return
+    }
+  })
+  return newArr.join("")
+}
 
-
-
+// Sanity check for noVowels()
+// console.log(noVowels(testString));
 
 
 // 4b. STRETCH: Update your function to return an error message if the variable is not a string. Copy and paste your function to add these changes.
-
-
-
-
-
+const noVowelsPlus = (arg1) => {
+  if (typeof arg1 === "string") {
+    let newArr = arg1.split("")
+    newArr = newArr.filter((v) => {
+      if (v !== "a" && v!== "e" && v !== "i" && v !== "o" && v !=="u") {
+        return v
+      } else {
+        return
+      }
+    })
+    return newArr.join("")
+  } else {
+      return `Error: "${arg1}" entered, expected a string`
+  }
+}
+// Sanity checks for noVowelsPlus()
+// console.log(noVowelsPlus(testString));
+// console.log(noVowelsPlus(4));
 
 // ------------------------------- Consider the following variable:
-
-var toonimals = [ {name: "Itchy", animal: "mouse"}, {name: "Stimpy", animal: "cat"}, {name: "Daffy", animal: "duck"}, {name: "Scratchy", animal: "cat"}, {name: "Ren", animal: "dog"}, {name: "Felix", animal: "cat"}]
+var toonimals = [ {name: "Itchy",     animal: "mouse"},
+                  {name: "Stimpy",    animal: "cat"},
+                  {name: "Daffy",     animal: "duck"},
+                  {name: "Scratchy",  animal: "cat"},
+                  {name: "Ren",       animal: "dog"},
+                  {name: "Felix",     animal: "cat"}]
 
 // 5a. Write a function that takes in the toonimals array as an argument and returns an array with only the toon objects that are cats. Expected output: [ { name: "Stimpy", animal: "cat" }, { name: "Scratchy", animal: "cat" }, { name: "Felix", animal: "cat" } ]
-
-
-
+const onlyCats = (array) => {
+  let cats = array.filter((v, i) => {
+    if (v.animal === "cat") {
+      return v
+    } else {
+      return
+    }
+  })
+  console.log(cats);
+}
+// Sanity check for onlyCats()
+// onlyCats(toonimals)
 
 //5b. Write a function that returns only the names of all the non-cats. Expected output: "Itchy" "Daffy" "Ren"
-
-
-
+const notCats = (array) => {
+  let otherAnimals = array.filter((v, i) => {
+    if (v.animal !== "cat") {
+      console.log(v.name);
+      return v.name
+    } else {
+      return
+    }
+  })
+}
+// Sanity check for notCats()
+// notCats(toonimals)
 
 //5c. STRETCH: Create a function that will print a sentence for every toon stating their name and their animal type. Use string interpolation. Expected output: "Itchy is a mouse" "Stimpy is a cat" "Daffy is a duck" "Scratchy is a cat" "Ren is a dog" "Felix is a cat"
+
+const aboutToon = (array) => {
+  let toons = array.map((v) => {
+    console.log(`${v.name} is a ${v.animal}`);
+})
+}
+// Sanity check for aboutToons()
+// aboutToon(toonimals)
--- js-intro.md
+++ js-intro.md
@@ -7,47 +7,159 @@ Answer the following questions. First, without external resources. Challenge you
 
   Your answer:
 
+    A function is a piece of code that "does" something.
+    A function may need to be passed in arguments to work.
+    A method is a function.
+    There are many methods that can assess or manipulate data that come with vanilla JS.
+
   Researched answer:
 
+    Functions are one of the fundamental building blocks in JavaScript. A function is a JavaScript procedure—a set of statements that performs a task or calculates a value. To use a function, you must define it somewhere in the scope from which you wish to call it.
 
+    source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions
 
 2. List 5 examples of JavaScript array methods.
 
   Your answer:
 
+    .join() -- joins an array into a string
+
+    .concat() -- adds together two arrays
+
+    .push() -- puts a value at the end of an array
+
+    .shift() -- removes the first value of an array
+
+    .unshift() -- puts a value to the start of an array
+
   Researched answer:
 
+    concat()	Joins two or more arrays, and returns a copy of the joined arrays
 
+    copyWithin()	Copies array elements within the array, to and from specified positions
+
+    entries()	Returns a key/value pair Array Iteration Object
+
+    every()	Checks if every element in an array pass a test
+
+    fill()	Fill the elements in an array with a static value
+
+    filter()	Creates a new array with every element in an array that pass a test
+
+    find()	Returns the value of the first element in an array that pass a test
+
+    findIndex()	Returns the index of the first element in an array that pass a test
+
+    forEach()	Calls a function for each array element
+
+    from()	Creates an array from an object
+
+    includes()	Check if an array contains the specified element
+
+    indexOf()	Search the array for an element and returns its position
+
+    isArray()	Checks whether an object is an array
+
+    join()	Joins all elements of an array into a string
+
+    keys()	Returns a Array Iteration Object, containing the keys of the original array
+
+    lastIndexOf()	Search the array for an element, starting at the end, and returns its position
+
+    map()	Creates a new array with the result of calling a function for each array element
+
+    pop()	Removes the last element of an array, and returns that element
+
+    push()	Adds new elements to the end of an array, and returns the new length
+
+    reduce()	Reduce the values of an array to a single value (going left-to-right)
+
+    reduceRight()	Reduce the values of an array to a single value (going right-to-left)
+
+    reverse()	Reverses the order of the elements in an array
+
+    shift()	Removes the first element of an array, and returns that element
+
+    slice()	Selects a part of an array, and returns the new array
+
+    some()	Checks if any of the elements in an array pass a test
+
+    sort()	Sorts the elements of an array
+
+    splice()	Adds/Removes elements from an array
+
+    toString()	Converts an array to a string, and returns the result
+
+    unshift()	Adds new elements to the beginning of an array, and returns the new length
+
+    valueOf()	Returns the primitive value of an array
+
+    source: https://www.w3schools.com/jsref/jsref_obj_array.asp
 
 3. Explain the difference between console.log() and return.
 
   Your answer:
 
+    console.log() -- Prints to the console the value of the passed in argument.
+                     .log() is a method of the console.
+                     Often used to see if a function is "returning" the correct value.
+
+    return        -- The value a function provides when ran.
+                     Typically every function will have a return.
+                     Code after the return typically will not be run.
+
+                     Really, they are two very different things.
+
   Researched answer:
 
+    So, console.log is a function that logs the arguments you pass to the web console. return is a statement which specifies the value returned from a function.
 
+    No, they do not do the same thing.    
+
+    source: https://www.reddit.com/r/learnjavascript/comments/1wkl8h/consolelog_vs_return/ (top answer)
 
 4. In regards to functions, what is an argument? What is the difference between an argument and a variable?
 
   Your answer:
 
+    argument -- An argument is a piece of data needed for a function to run.
+                An argument is what is passed into a function between the ().
+                A function can be declared to take in any number of arguments.
+                The below function takes two arguments: num1 & num2.
+                          const multiply = (num1, num2) => {
+                                return num1 * num2
+                          }
+
+    variable -- A variable is reference to a piece of data that can be declared either inside or outside of a
+                function. A variable can be passed in as an argument.
+
   Researched answer:
 
+    Parameters are variables listed as a part of the function definition. Arguments are values passed to the function when it is invoked.
 
+    source: https://codeburst.io › parameters-arguments-in-javascript-eb1d8bd0ef04
 
 5. Give a brief description of proper pair programming techniques. What are the roles of each person?
 
   Your answer:
 
+    Pair programming is a technique designed to optimize code quality by having two individuals work together at one workstation. Generally, partners are divided into two responsibilities, the "driver" & the "navigator", and switch roles in some predefined way. The driver is the individual doing the physical input of typing the code. The navigator is responsible for guiding the driver through the logical operation & goals of the code. The navigator is also is entrusted to be a second sense of eyes to spot any errors or sloppiness in the driver's code. Pair programming is also helpful in ensuring that both parties stay engaged in production and not distracted.
+
   Researched answer:
 
+    Pair programming is an agile software development technique in which two programmers work together at one workstation. One, the driver, writes code while the other, the observer or navigator, reviews each line of code as it is typed in. The two programmers switch roles frequently.
 
+    source: https://en.wikipedia.org/wiki/Pair_programming
 
 6. Give a brief description of JavaScript as if you were explaining it to someone who doesn't know anything about programming.
 
   Your answer:
 
+    JavaScript is one of the most popular modern programming languages. Just about every modern website uses JavaScript (in combination with HTML & CSS) to function, including FaceBook, Google, & Amazon. It is a highly abstracted programming language essentially meaning it is a set of logical instructions written in englishish that then gets translated down to binary (0s and 1s) that your computer can understand. Because JavaScript is so popular, you can find many pre-built functionalities for a wide variety of tasks built by other programmers that you can slot into your own program. This is one major advantage of using JavaScript vs using a less popular language.
+
 
 7. What is one thing we did in class this week you found helpful?  
 
   Your answer:
+
+    I really enjoy the stretch challenges that force me to integrate the concept of the lesson with previous concepts.
